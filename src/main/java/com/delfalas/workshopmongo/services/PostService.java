2fb302ad6eaf169a8abe9ad4342d44796a18bbc4
Delfalas <vinidantas07@hotmail.com>
2025-04-03 17:28:06 -0300

    Obtendo um post por id

---
+++ src/main/java/com/delfalas/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.delfalas.workshopmongo.services;
+ 
+ import java.util.Optional;
+ 
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.stereotype.Service;
+ 
+ import com.delfalas.workshopmongo.domain.Post;
+ import com.delfalas.workshopmongo.repository.PostRepository;
+ import com.delfalas.workshopmongo.services.exception.ObjectNotFoundException;
+ 
+ @Service
+ public class PostService {
+ 
+ 	@Autowired
+ 	private PostRepository repo;
+ 	
+ 	public Post findById(String id) {
+ 		Optional<Post> obj = repo.findById(id);
+ 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+ 	}
+ }
