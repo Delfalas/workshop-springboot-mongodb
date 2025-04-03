badbe4b62ccbb139360f274fce087fdd88b14ab4
Delfalas <vinidantas07@hotmail.com>
2025-04-02 22:14:12 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/delfalas/workshopmongo/config/Instantiation.java
+++ src/main/java/com/delfalas/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.delfalas.workshopmongo.domain.Post;
 import com.delfalas.workshopmongo.domain.User;
+import com.delfalas.workshopmongo.dto.AuthorDTO;
 import com.delfalas.workshopmongo.repository.PostRepository;
 import com.delfalas.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User dani = new User(null, "Danielle Arcanjo", "daniarcanjo@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", vini);
- 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", vini);
-		
 		userReposiroty.saveAll(Arrays.asList(vini, dani, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(vini));
+ 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(vini));
+		
 		postReposiroty.saveAll(Arrays.asList(post1, post2));
 		
 	}
